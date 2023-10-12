f357708676d33c6898dbf1fc29b9e909878c7c0d
fawzi ouaheb <fawziouaheb@gmail.com>
2023-10-12 16:12:14 +0200

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.myapplication"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.myapplication"
+        minSdk = 27
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment:2.5.3")
+    implementation("androidx.navigation:navigation-ui:2.5.3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+}
+dependencies {
+    val sqlite_version = "2.3.0"
+
+    // Java language implementation
+    implementation("androidx.sqlite:sqlite:$sqlite_version")
+
+    // Kotlin
+    implementation("androidx.sqlite:sqlite-ktx:$sqlite_version")
+
+    // Implementation of the AndroidX SQLite interfaces via the Android framework APIs.
+    implementation("androidx.sqlite:sqlite-framework:$sqlite_version")
+}
+dependencies {
+    val room_version = "2.5.0"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+}
